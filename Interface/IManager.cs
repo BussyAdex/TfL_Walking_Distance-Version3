b04ea5432a42f1b1b00bb05ca0da171493a14f99
Bussyadex <Bussyadex42@gmail.com>
2024-01-29 01:24:32 +0000

    first commit

---
+++ Interface/IManager.cs
@@ -0,0 +1,15 @@
+﻿using TfL_Walking_Distance_Version3.Model;
+
+namespace TfL_Walking_Distance_Version3.Interface
+{
+    internal interface IManager
+    {
+        public bool SetDelayTime(string sName, string dName, int t, string rs);
+
+        public bool SetRouteUpdate(string sName, string dName, STATUS st);
+
+        public void PrintImposible();
+
+        public void PrintDelayed();
+    }
+}
